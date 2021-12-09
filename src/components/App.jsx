6ac2379deb44ea76c871ba7c350861a0b8ec6266
Jimmy J. Pecho Malqui <jpmjim@gmail.com>
2021-12-08 22:10:30 -0500

    creacion del logo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
 import '../styles/global.scss';
-import Photo from '../assets/images/profile-pic.png';
+import Photo from '../assets/images/logo.png';
 
 
 const App = () => {
   return (
     <div>
-      <header className="header">
-        <h1><a href='index'>JIMMY P.</a></h1>
-        <ul className='header_menu'>
+      <header className='header'>
+        <a className='header__logo' href="">J2P</a>
+        <ul className='navigation'>
           <li><a href="">Home</a></li>
           <li><a href="">About</a></li>
           <li><a href="">Portfolio</a></li>
           <li><a href="">News</a></li>
           <li><a href="">Contact</a></li>
         </ul>
+        <img src={Photo} alt="" />
       </header>
-      <main>
-        <img className="header__img" src={Photo} alt="Jimmy Joel Pecho Malqui" />
-        <h2>Jimmy J. PECHO MALQUI</h2>
-      </main>
     </div>
   );
 }
