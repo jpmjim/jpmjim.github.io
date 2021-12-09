d0796a7fecefe3119fa85f79d3566fe7092a57f7
Jimmy J. Pecho Malqui <jpmjim@gmail.com>
2021-12-08 22:46:25 -0500

    estelizando el menu

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,15 +7,14 @@ const App = () => {
   return (
     <div>
       <header className='header'>
-        <a className='header__logo' href="">J2P</a>
-        <ul className='navigation'>
+        <a  className='header__logo' href=""><img src={Photo} alt=""/></a>
+        <ul className='header__navigation'>
           <li><a href="">Home</a></li>
           <li><a href="">About</a></li>
           <li><a href="">Portfolio</a></li>
           <li><a href="">News</a></li>
           <li><a href="">Contact</a></li>
         </ul>
-        <img src={Photo} alt="" />
       </header>
     </div>
   );
