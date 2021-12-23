5971ab362cf049e1a8dc4f5a4aae19690d191387
Jimmy J. Pecho Malqui <jpmjim@gmail.com>
2021-12-23 17:03:24 -0500

    responsive menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,14 @@ import '../styles/Header.scss';
 
 const Header = () => {
   return (
-    <nav>
-      <a href=""><img src={Logo} alt="" /></a>
-      <ul className="text-rose-600">
+    <nav className='header'>
+      <a href=""><img className='header__logo' src={Logo} alt=""/></a>
+      <ul className='header__list'>
         <li><a href="/">Home</a></li>
         <li><a href="About">About</a></li>
         <li><a href="Portfolio">Portfolio</a></li>
       </ul>
+      <button className='header__menu' href=""><i className="bi bi-list"></i></button>
     </nav>
   );
 }
--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Layout = ({ children }) => {
   return (
-    <div className='Layout '>
+    <div className='Layout'>
       {children}
     </div>
   );
